ec6fefe2cd86e17f67bb75d505021028d651493a
Jordan Dodson <jordandodson22@gmail.com>
2017-08-05 14:48:50 -0500

    simplification now performed on init. adding other basic functionality

--- Expressions.py
+++ Expressions.py
@@ -1,11 +1,15 @@
 #AUTH: Jordan Dodson
 
 #TODO (x == complete | . == in progress | - == canceled)
-# (.) _simplified() for Add class
-# ( ) _simplified() for Mult class
+# (.) _simplify() for Add class
+# ( ) _simplify() for Mult class
 # (x) Write a Const class
 # ( ) Neg function or use a special method
 # ( ) simplify on initialization
+# ( ) Write a static .parse() method
+# ( ) Introduce basic trig functions
+# ( ) Write __str__() methods for all classes
+# ( ) Consider using __new__ to handle corner-cases in simplification (i.e. Mult(1, 'x'))
 
 class Expression():
   '''The base class for all symbolic expressions'''
@@ -14,24 +18,39 @@ class Expression():
     # The set of labels in this expression
     self.labels = set()
 
+  # Unary operations
+
+  def __pos__(self):
+    return self
+
+  def __neg__(self):
+    return self.neg()
+
   def __add__(self, other):
-    z = Add(self, other)
-    return z._simplified()
+    return Add(self, other)
+
+  def __radd__(self, other):
+    return Add(self, other)
 
   def __mul__(self, other):
-    z = Mult(self, other)
-    return z._simplified()
+    return Mult(self, other)
 
-  def deriv(self, wrt=None):
+  def __rmul__(self, other):
+    return Mult(self, other)
+
+  def neg(self):
     pass
 
-  def integral(self, wrt=None):
+  def deriv(self, wrt):
+    pass
+
+  def integral(self, wrt):
     pass
 
   def evaluate(self, value_dict):
     pass
 
-  def _simplified(self):
+  def _simplify(self):
     pass
 
 
@@ -41,33 +60,51 @@ class Const(Expression):
     self.labels = set()
     self.value = value
 
-  def evaluate(self, value_dict):
+  def neg(self):
+    return Const(-self.value)
+
+  def evaluate(self, **value_dict):
     return self
 
-  def _simplified(self):
-    return self
+  def deriv(self, wrt):
+    return Const(0)
+
+  def __str__(self):
+    return str(self.value)
 
   def __repr__(self):
     return 'Const({})'.format(self.value)
 
 
 class Symbol(Expression):
-   '''Represents a single symbol'''
+  '''Represents a single symbol'''
 
-   def __init__(self, label):
+  def __init__(self, label):
 
-     self.labels = set([label])
+    self.labels = set([label])
 
-     if isinstance(label, str) and label.isalpha():
-       self.label = label
-     else:
-       raise Exception('Invalid symbol label')
+    if isinstance(label, str) and label.isalpha():
+      self.label = label
+    else:
+      raise Exception('Invalid symbol label')
 
-   def evaluate(self, value_dict):
-     return value_dict.get(self.label, self)
+  def neg(self):
+    return Mult(Const(-1), self)
 
-   def __repr__(self):
-     return 'Symbol({:s})'.format(self.label)
+  def evaluate(self, **value_dict):
+    if self.label in value_dict:
+      return Const(value_dict[self.label])
+    else:
+      return self
+
+  def deriv(self, wrt):
+    return Const(1) if self.label == wrt else Const(0)
+
+  def __str__(self):
+    return self.label
+
+  def __repr__(self):
+    return 'Symbol({:s})'.format(self.label)
 
 
 class Add(Expression):
@@ -77,17 +114,32 @@ class Add(Expression):
 
     self.terms = [a,b] + list(rest)
 
-    # Get the set of labels in this Expression
-    self.labels = set()
-    for term in self.terms:
-      if isinstance(term, Expression):
-        self.labels.update(term.labels)
+    self._simplify()
 
-  def evaluate(self, value_dict):
+
+  def evaluate(self, **value_dict):
     z = [term.evaluate(value_dict) for term in self.terms]
-    return Add(*z)._simplified()
+    return Add(*z)
 
-  def _simplified(self):
+  def deriv(self, wrt):
+
+    if wrt in self.labels:
+      z = [term.deriv(wrt) for term in self.terms]
+      return Add(*z)
+    else:
+      return Const(0)
+
+  def _get_labels(self):
+
+    labels = set()
+
+    for term in self.terms:
+      labels.update(term.labels)
+
+    return labels
+    
+
+  def _simplify(self):
 
     # List of sub-expressions to check
     to_check = self.terms.copy()
@@ -113,10 +165,17 @@ class Add(Expression):
     if offset != 0:
       simplified.append(Const(offset))
 
-    return Add(*simplified) if len(simplified) > 1 else simplified[0]
+    # The simplified terms of this Add
+    self.terms = simplified
+
+    # Recompute the set of labels in this Expression
+    self.labels = self._get_labels()
+
+  def __str__(self):
+    return ' + '.join(str(term) for term in self.terms)
 
   def __repr__(self):
-    return 'Add({:s})'.format(', '.join(str(term) for term in self.terms))
+    return 'Add({:s})'.format(', '.join(repr(term) for term in self.terms))
       
 
 class Mult(Expression):
@@ -130,11 +189,17 @@ class Mult(Expression):
       if isinstance(term, Expression):
         self.labels.update(term.labels)
 
-  def _simplified(self):
+  def neg(self):
+    return Mult(Const(-1), self)
+
+  def _simplify(self):
     pass
 
+  def __str__(self):
+    return ''.join(str(term) for term in self.terms)
+
   def __repr__(self):
-    return 'Mult({:s})'.format(', '.join(str(term) for term in self.terms))
+    return 'Mult({:s})'.format(', '.join(repr(term) for term in self.terms))
 
 
 class Exp(Expression):
